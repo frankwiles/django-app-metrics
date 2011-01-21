baec2c8e545b6c26cb1de8bff0607974a659034d
Ross Poulton <ross@rossp.org>
2011-01-21 05:47:07 +0000

    Simplify admin.py and add Admin definition for MetricItem model

--- app_metrics/admin.py
+++ app_metrics/admin.py
@@ -2,32 +2,10 @@ from django.contrib import admin
 
 from app_metrics.models import Metric, MetricSet, MetricItem, MetricDay, MetricWeek, MetricMonth, MetricYear
 
-class MetricAdmin(admin.ModelAdmin): 
-    model = Metric 
-
-admin.site.register(Metric, MetricAdmin)
-
-class MetricSetAdmin(admin.ModelAdmin): 
-     model = MetricSet
-
-admin.site.register(MetricSet, MetricSetAdmin) 
-
-class MetricDayAdmin(admin.ModelAdmin): 
-    model = MetricDay 
-
-admin.site.register(MetricDay, MetricDayAdmin) 
-
-class MetricWeekAdmin(admin.ModelAdmin): 
-    model = MetricWeek
-
-admin.site.register(MetricWeek, MetricWeekAdmin) 
-
-class MetricMonthAdmin(admin.ModelAdmin): 
-    model = MetricMonth
-
-admin.site.register(MetricMonth, MetricMonthAdmin) 
-
-class MetricYearAdmin(admin.ModelAdmin): 
-    model = MetricYear
-
-admin.site.register(MetricYear, MetricYearAdmin) 
+admin.site.register(Metric)
+admin.site.register(MetricSet)
+admin.site.register(MetricDay)
+admin.site.register(MetricWeek)
+admin.site.register(MetricMonth)
+admin.site.register(MetricYear) 
+admin.site.register(MetricItem) 
