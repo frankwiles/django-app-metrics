e8e5d5364dfc1f2ea30d1d3b229392271d59ccd1
Flavio Curella <flavio.curella@gmail.com>
2011-07-11 14:40:25 -0500

    moved tasks into tasks.py
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- app_metrics/backends/mixpanel.py
+++ app_metrics/backends/mixpanel.py
@@ -1,15 +1,9 @@
 # Backend to handle sending app metrics directly to mixpanel.com 
 # See http://mixpanel.com/api/docs/ for more information on their API 
 
-import base64
-import json 
-import urllib
-import urllib2
-
-from celery.decorators import task 
-
 from django.conf import settings 
 from django.core.exceptions import ImproperlyConfigured
+from app_metrics.tasks import mixpanel_metric_task
 
 def _get_token(): 
     token = getattr(settings, 'APP_METRICS_MIXPANEL_TOKEN', None) 
@@ -33,25 +27,3 @@ def metric(slug, num=1, properties=None):
     """
     token = _get_token()
     mixpanel_metric_task.delay(slug, num, properties) 
-
-@task 
-def mixpanel_metric_task(slug, num, properties=None, **kwargs):
-
-    token = _get_token()
-    if properties == None: 
-        properties = dict() 
-
-    if "token" not in properties: 
-        properties["token"] = token 
-
-    url = getattr(settings, 'APP_METRICS_MIXPANEL_API_URL', "http://api.mixpanel.com/track/")
-
-    params = {"event": slug, "properties": properties}
-    b64_data = base64.b64encode(json.dumps(params))
-
-    data = urllib.urlencode({"data": b64_data})
-    for i in range(num):
-        req = urllib2.Request(url, data) 
-        response = urllib2.urlopen(req) 
-
-
