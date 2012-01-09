7b874fbddd66957c095da24faa60cae859b484fe
Jannis Leidel <jannis@leidel.info>
2012-01-09 18:06:07 +0100

    Made sure all files are properly packaged on all Python versions.

--- setup.py
+++ setup.py
@@ -15,8 +15,13 @@ setup(
     long_description=readme,
     author='Frank Wiles',
     author_email='frank@revsys.com',
-    url='http://github.com/frankwiles/django-app-metrics/tree/master',
+    url='https://github.com/frankwiles/django-app-metrics',
     packages=find_packages(),
+    package_data={
+        'app_metrics': [
+            'templates/app_metrics/*',
+        ]
+    },
     install_requires = [
         'celery',
         'django-celery',
