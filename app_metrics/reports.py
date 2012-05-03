f526055e7779086520ca508ef17f3d2d738465fa
Frank Wiles <frank@revsys.com>
2012-05-03 10:07:42 -0500

    Whitespace cleanup

--- app_metrics/reports.py
+++ app_metrics/reports.py
@@ -1,18 +1,18 @@
-import datetime 
+import datetime
 
-from django.template.loader import render_to_string 
+from django.template.loader import render_to_string
 
-from app_metrics.models import * 
-from app_metrics.trending import trending_for_metric 
+from app_metrics.models import *
+from app_metrics.trending import trending_for_metric
 
 from django.conf import settings
 
-def generate_report(metric_set=None, html=False): 
-    """ Generate a Metric Set Report """ 
+def generate_report(metric_set=None, html=False):
+    """ Generate a Metric Set Report """
 
-    # Get trending data for each metric 
+    # Get trending data for each metric
     metric_trends = []
-    for m in metric_set.metrics.all(): 
+    for m in metric_set.metrics.all():
         data = {'metric': m}
         data['trends'] = trending_for_metric(m)
         metric_trends.append(data)
@@ -28,21 +28,21 @@ def generate_report(metric_set=None, html=False):
         if not activity_today:
             return None, None
 
-   
-    message = render_to_string('app_metrics/email.txt', { 
-                            'metric_set': metric_set, 
-                            'metrics': metric_trends, 
+
+    message = render_to_string('app_metrics/email.txt', {
+                            'metric_set': metric_set,
+                            'metrics': metric_trends,
                             'today': datetime.date.today(),
                 })
 
-    if html: 
-        message_html = render_to_string('app_metrics/email.html', { 
-                            'metric_set': metric_set, 
-                            'metrics': metric_trends, 
+    if html:
+        message_html = render_to_string('app_metrics/email.html', {
+                            'metric_set': metric_set,
+                            'metrics': metric_trends,
                             'today': datetime.date.today(),
                 })
 
-        return message, message_html 
+        return message, message_html
 
-    else: 
-        return message 
+    else:
+        return message
