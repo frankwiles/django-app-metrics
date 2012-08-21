2e6eed9083fa3bce2350afb81a55e83e378942da
Hannes Struss <x@hannesstruss.de>
2012-08-21 16:24:21 +0200

    implement complete metrics api in librato

--- app_metrics/backends/librato_backend.py
+++ app_metrics/backends/librato_backend.py
@@ -1,5 +1,14 @@
 from app_metrics.tasks import librato_metric_task
 
 
-def metric(name, num=1, **kwargs):
-    librato_metric_task.delay(name, num, **kwargs)
+def metric(slug, num=1, **kwargs):
+    librato_metric_task.delay(slug, num, metric_type="counter", **kwargs)
+
+
+def timing(slug, seconds_taken, **kwargs):
+    """not implemented"""
+
+
+def gauge(slug, current_value, **kwargs):
+    librato_metric_task.delay(slug, current_value, metric_type="gauge",
+                              **kwargs)
