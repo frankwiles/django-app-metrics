59358f74f85d6e06f5810e5246d750de1ae6f703
Hannes Struss <x@hannesstruss.de>
2012-08-29 16:44:35 +0200

    allow to synchronously call librato task

--- app_metrics/backends/librato_backend.py
+++ app_metrics/backends/librato_backend.py
@@ -1,14 +1,20 @@
 from app_metrics.tasks import librato_metric_task
 
 
-def metric(slug, num=1, **kwargs):
-    librato_metric_task.delay(slug, num, metric_type="counter", **kwargs)
+def metric(slug, num=1, async=True, **kwargs):
+    if async:
+        librato_metric_task.delay(slug, num, metric_type="counter", **kwargs)
+    else:
+        librato_metric_task(slug, num, metric_type="counter", **kwargs)
 
 
-def timing(slug, seconds_taken, **kwargs):
+def timing(slug, seconds_taken, async=True, **kwargs):
     """not implemented"""
 
 
-def gauge(slug, current_value, **kwargs):
-    librato_metric_task.delay(slug, current_value, metric_type="gauge",
-                              **kwargs)
+def gauge(slug, current_value, async=True, **kwargs):
+    if async:
+        librato_metric_task.delay(slug, current_value, metric_type="gauge",
+                                  **kwargs)
+    else:
+        librato_metric_task(slug, current_value, metric_type="gauge", **kwargs)
