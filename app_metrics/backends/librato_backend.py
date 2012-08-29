b904ad9ef3da27bf4c2ee2aa2917048f949c8a8f
Hannes Struss <x@hannesstruss.de>
2012-08-29 16:49:28 +0200

    be more expressive

--- app_metrics/backends/librato_backend.py
+++ app_metrics/backends/librato_backend.py
@@ -1,11 +1,12 @@
 from app_metrics.tasks import librato_metric_task
 
 
+def _get_func(async):
+    return librato_metric_task.delay if async else librato_metric_task
+
+
 def metric(slug, num=1, async=True, **kwargs):
-    if async:
-        librato_metric_task.delay(slug, num, metric_type="counter", **kwargs)
-    else:
-        librato_metric_task(slug, num, metric_type="counter", **kwargs)
+    _get_func(async)(slug, num, metric_type="counter", **kwargs)
 
 
 def timing(slug, seconds_taken, async=True, **kwargs):
@@ -13,8 +14,4 @@ def timing(slug, seconds_taken, async=True, **kwargs):
 
 
 def gauge(slug, current_value, async=True, **kwargs):
-    if async:
-        librato_metric_task.delay(slug, current_value, metric_type="gauge",
-                                  **kwargs)
-    else:
-        librato_metric_task(slug, current_value, metric_type="gauge", **kwargs)
+    _get_func(async)(slug, current_value, metric_type="gauge", **kwargs)
