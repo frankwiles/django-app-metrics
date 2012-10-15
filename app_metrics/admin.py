3341cc438d48b1254f58c2dec2356200351e15fb
Vineet Naik <vineet.naik@kodeplay.com>
2012-10-15 15:53:56 +0530

    Add filtering by metric name in admin
    Added filter by metric functionality for MetricDay, MetricWeek,
    MetricMonth and MetricYear ModelAdmins

--- app_metrics/admin.py
+++ app_metrics/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 from app_metrics.models import Metric, MetricSet, MetricItem, MetricDay, MetricWeek, MetricMonth, MetricYear
 
+class MetricAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'slug', 'num')
+    list_filter = ['metric__name']
+
+    def slug(self, obj):
+        return obj.metric.slug
+
 admin.site.register(Metric)
 admin.site.register(MetricSet)
-admin.site.register(MetricDay)
-admin.site.register(MetricWeek)
-admin.site.register(MetricMonth)
-admin.site.register(MetricYear)
-admin.site.register(MetricItem)
+admin.site.register(MetricDay, MetricAdmin)
+admin.site.register(MetricWeek, MetricAdmin)
+admin.site.register(MetricMonth, MetricAdmin)
+admin.site.register(MetricYear, MetricAdmin)
+admin.site.register(MetricItem, MetricAdmin)
