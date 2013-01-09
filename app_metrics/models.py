a2571baf48f585b666c3dace70c1c963fe801fec
Jason Michalski <jmichalski@ruelala.com>
2013-01-09 09:06:32 -0500

    Add support for custom user models with django 1.5

--- app_metrics/models.py
+++ app_metrics/models.py
@@ -1,9 +1,11 @@
 import datetime
 
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models, IntegrityError
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
+
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Metric(models.Model):
@@ -35,7 +37,7 @@ class MetricSet(models.Model):
     """ A set of metrics that should be sent via email to certain users """
     name = models.CharField(_('name'), max_length=50)
     metrics = models.ManyToManyField(Metric, verbose_name=_('metrics'))
-    email_recipients = models.ManyToManyField(User, verbose_name=_('email recipients'))
+    email_recipients = models.ManyToManyField(USER_MODEL, verbose_name=_('email recipients'))
     no_email = models.BooleanField(_('no e-mail'), default=False)
     send_daily = models.BooleanField(_('send daily'), default=True)
     send_weekly = models.BooleanField(_('send weekly'), default=False)
